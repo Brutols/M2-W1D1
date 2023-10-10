16e75ce63971307d2c4abbf55a8a28217b545d32
Brutols <dtwo97@gmail.com>
2023-10-10 17:05:35 +0200

    test file with questions and answers

---
+++ script.js
@@ -0,0 +1,29 @@
+let question1 = "Cos'è un algoritmo?"
+
+let answer1 = "un algoritmo è una serie di passaggi o azioni, spesso automatizzate almeno in informatica, che servono per risolvere un problema o un obiettivo che si vuole raggiungere, in questo caso l'algoritmo sarà: cosa devo fare per fare in modo che accada questa determinata cosa? es:voglio mettere in moto la macchina (risultato) premere frizione e/o freno + girare la chiave (algoritmo)"
+
+let firstAnswer
+
+firstAnswer = (question1 + answer1)
+
+console.log(firstAnswer)
+
+let question2 = "Cos'è una variabile?"
+
+let answer2 = "Una variabile è un contenitore di valori (lettere, frasi, numeri, somme/sottrazioni ecc di altre variabili...) è paragonabile ad un ingrediente di una ricetta per es. sale può essere una variabile ed è appunto variabile perchè l'ingrediente sale rimane lo stesso ma in ricette differenti me ne serviranno quantità diverse quindi può avere valori diversi, variabili"
+
+let secondAnswer
+
+secondAnswer = (question2 + answer2)
+
+console.log(secondAnswer)
+
+let question3 = "undefined e null sono la stessa cosa?"
+
+let answer3 = "No undefined indica un valore non dichiarato, una variabile non definita, senza valore assegnato. Mentre null è un valore che può essere assegnato ad una variabile e rappresenta (nessun valore) null verrà letto da javascript come tipo oggetto mentre undefined verrà letto come tipo a se stante, appunto, undefined"
+
+let thirdAnswer
+
+thirdAnswer = (question3 + answer3)
+
+console.log(thirdAnswer)
